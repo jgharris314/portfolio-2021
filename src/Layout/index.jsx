1662aa7f60bbd94415ec99c5f8bc2818b3e45dac
Jacob Harris <jgharris314@gmail.com>
2021-06-10 12:45:48 -0600

    cleaning things up

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,125 +1,115 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 // import { Route, Switch, useParams } from "react-router-dom";
-import Header from '../Header/Header'
-import Navigation from'../Navigation/Navigation'
-import Body from '../Body/Body'
-import ColorPicker from '../ColorPicker/ColorPicker'
-import './index.css'
-export default function Layout () {
-
-    const [headerStyles, setHeaderStyles] = useState({
-        h1: { color: "green" },
-        h2: { color: "orange" },
-        h3: { color: "purple" },
-        background: { "background-color": "#282c34" }
-      })
-
-      const [bodyStyles, setBodyStyles] = useState({
-        "background-color": "#282c34",
-        "color": "white"
-      })
-
-    const styleOneHeaderStyle = {
+import Header from "../Header/Header";
+import Navigation from "../Navigation/Navigation";
+import Body from "../Body/Body";
+import ColorPicker from "../ColorPicker/ColorPicker";
+import "./index.css";
+export default function Layout() {
+  const [styles, setStyles] = useState({
+    header: {
       h1: { color: "#baee4c" },
       h2: { color: "#ce91d8" },
       h3: { color: "#84428f" },
-      background: { "background-color": "#210624" }
-    };
+      background: { "background-color": "#210624" },
+    },
+    body: {
+      background: { "background-color": "black", color: "red" },
+      h3: { color: "grey" },
+    },
+  });
 
-    const styleTwoHeaderStyle = {
-        h1: { color: "#87ceeb" },
-        h2: { color: "#5e1224" },
-        h3: { color: "#dcdcdb" },
-        background: { "background-color": "#eebbf5" }
-      };
+  const styleOne = {
+    header: {
+      h1: { color: "#baee4c" },
+      h2: { color: "#ce91d8" },
+      h3: { color: "#84428f" },
+      background: { "background-color": "#210624" },
+    },
+    body: {
+      background: { "background-color": "black", color: "red" },
+      h3: { color: "grey" },
+    },
+  };
 
-      const styleThreeHeaderStyle = {
-        h1: { color: "#afb5fc" },
-        h2: { color: "#e76aa3" },
-        h3: { color: "#065535" },
-        background: { "background-color": "#f94552" }
-      };
+  const styleTwo = {
+    header: {
+      h1: { color: "#87ceeb" },
+      h2: { color: "#5e1224" },
+      h3: { color: "#dcdcdb" },
+      background: { "background-color": "#eebbf5" },
+    },
+    body: {
+      background: { "background-color": "grey", color: "orange" },
+      h3: { color: "green" },
+    },
+  };
 
-      const styleFourHeaderStyle = {
-        h1: { color: "#a020f0" },
-        h2: { color: "#bada55" },
-        h3: { color: "#cc99cc" },
-        background: { "background-color": "#008080" }
-      };
+  const styleThree = {
+    header: {
+      h1: { color: "#afb5fc" },
+      h2: { color: "#e76aa3" },
+      h3: { color: "#065535" },
+      background: { "background-color": "#f94552" },
+    },
+    body: {
+    background: { "background-color": "black", color: "green" },
+    h3: { color: "yellow" },
+    }
+  };
 
-      const styleOneBodyStyle = {
-        background: { "background-color": "black", color: "red" },
-        h3: { color: "grey" },
-      };
+  const styleFour = {
+    header: {
+      h1: { color: "#a020f0" },
+      h2: { color: "#bada55" },
+      h3: { color: "#cc99cc" },
+      background: { "background-color": "#008080" },
+    },
+    body: {
+      background: { "background-color": "purple", color: "green" },
+      h3: { color: "orange" },
+    },
+  };
 
-      const styleTwoBodyStyle = {
-        background: { "background-color": "grey", color: "orange" },
-        h3: { color: "green" },
-      };
+  const styleSwitchOnClick = (id) => {
+    if (id === 1) {
+      setStyles(styleOne);
+    } else if (id === 2) {
+      setStyles(styleTwo);
+    } else if (id === 3) {
+      setStyles(styleThree);
+    } else if (id === 4) {
+      setStyles(styleFour);
+    }
+  };
 
-      const styleThreeBodyStyle = {
-        background: { "background-color": "black", color: "green" },
-        h3: { color: "yellow" },
-      };
-
-      const styleFourBodyStyle = {
-        background: { "background-color": "purple", color: "green" },
-        h3: { color: "orange" },
-      };
-
-      const styleSwitchOnClick = (id) => {
-        if (id === 1) {
-          setHeaderStyles(styleOneHeaderStyle);
-          setBodyStyles(styleOneBodyStyle);
-        } else if (id === 2) {
-          setHeaderStyles(styleTwoHeaderStyle);
-          setBodyStyles(styleTwoBodyStyle);
-        } else if (id === 3) {
-          setHeaderStyles(styleThreeHeaderStyle);
-          setBodyStyles(styleThreeBodyStyle);
-        } else if (id === 4) {
-          setHeaderStyles(styleFourHeaderStyle);
-          setBodyStyles(styleFourBodyStyle);
-        }
-      };
-
-      
-
-    return (
+  return (
     <div className="container layoutDiv">
-        <div className="row headerDiv">
-        <div  className="row styleButtons">
-                <div className="col col-3">
-                <button onClick={() => styleSwitchOnClick(1)}>Don't</button>
-                </div>
-                <div className="col col-3">
-                <button onClick={() => styleSwitchOnClick(2)}>Like</button>
-                </div>
-                <div className="col col-3">
-                <button onClick={() => styleSwitchOnClick(3)}>The</button>
-                </div>
-                <div className="col col-3">
-                <button onClick={() => styleSwitchOnClick(4)}>Colors?</button>
-                </div>
-            </div>
-            
-            <Header headerStyles={headerStyles}/>
+      <div className="row headerDiv">
+        <div className="row styleButtons">
+          <div className="col col-3">
+            <button onClick={() => styleSwitchOnClick(1)}>Don't</button>
+          </div>
+          <div className="col col-3">
+            <button onClick={() => styleSwitchOnClick(2)}>Like</button>
+          </div>
+          <div className="col col-3">
+            <button onClick={() => styleSwitchOnClick(3)}>The</button>
+          </div>
+          <div className="col col-3">
+            <button onClick={() => styleSwitchOnClick(4)}>Colors?</button>
+          </div>
         </div>
-        
-        <div className="row bodyDiv">
-        
-            <Body bodyStyles={bodyStyles}/>
-        </div>
-       
-            
-        
-        {/* <div className="ColorPickerDiv">
-            <ColorPicker />
-        </div> */}
-        <div className="row navDiv">
-            <Navigation />
-        </div>
-        
+
+        <Header headerStyles={styles.header} />
+      </div>
+
+      <div className="row bodyDiv">
+        <Body bodyStyles={styles.body} />
+      </div>
+      <div className="row navDiv">
+        <Navigation />
+      </div>
     </div>
-    )
+  );
 }
