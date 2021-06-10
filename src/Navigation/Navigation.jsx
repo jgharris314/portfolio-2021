ef044d200fd0880947f4bd1b43658b445faa6128
Jacob Harris <jgharris314@gmail.com>
2021-06-10 14:12:16 -0600

    trying to deploy

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -7,7 +7,7 @@ export default function Navigation () {
     return (
         <div className="container Navigation">
             <div className="row">
-                <div className="col col-3"><a>Home</a></div>
+                <div className="col col-3">Home</div>
                 <div className="col col-3"><a href="#about">About</a></div>
                 <div className="col col-3"><a href="#projects">Projects</a></div>
                 <div className="col col-3"><a href="#contact">Contact</a></div>
